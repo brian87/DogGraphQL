09d5c08a05aa01b0507e2471a11ad2c86c4b3d8c
tumi <tumi@Sherlock.local>
2021-02-18 10:10:39 +0000

    Creating Query and Mutation

---
+++ src/main/java/com/brain/dogGraphQL/resolver/Query.java
@@ -0,0 +1,33 @@
+package com.brain.dogGraphQL.resolver;
+
+import java.util.Optional;
+
+import org.springframework.stereotype.Component;
+
+import com.brain.dogGraphQL.entity.Dog;
+import com.brain.dogGraphQL.exception.DogNotFoundException;
+import com.brain.dogGraphQL.repository.DogRepository;
+
+@Component
+public class Query {
+	private DogRepository dogRepository;
+	
+	public Query(DogRepository dogRepository) {
+        this.dogRepository = dogRepository;
+    }
+	
+	public Iterable<Dog> findAllDogs() {
+        return dogRepository.findAll();
+ 
+    }
+	
+	public Dog findDogById(Long id) {
+		Optional<Dog> optionalDog = dogRepository.findById(id);
+		if (optionalDog.isPresent()) {
+            return optionalDog.get();
+        } else {
+            throw new DogNotFoundException("Dog Not Found", id);
+        }
+	}
+
+}
