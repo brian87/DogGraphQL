0ff3a8ad9a7a7b33bdad183de76e4ac957b8330b
tumi <tumi@Sherlock.local>
2021-02-18 12:59:57 +0000

    adding data.sql

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+
+INSERT INTO dog (id, name, breed, origin) VALUES (1, 'bobby1','pekin1','origin1');
+INSERT INTO dog (id, name, breed, origin) VALUES (2, 'bobby2','pekin2','origin2');
+INSERT INTO dog (id, name, breed, origin) VALUES (3, 'bobby3','pekin3','origin3');
+INSERT INTO dog (id, name, breed, origin) VALUES (4, 'bobby4','pekin4','origin4');
+INSERT INTO dog (id, name, breed, origin) VALUES (5, 'bobby5','pekin5','origin5');
